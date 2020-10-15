ae90f8ad543a274773fc6cdf0c3ec317746fe0a6
Chit Moe Wai <chitmoewai.360ed@gmail.com>
2020-10-15 17:44:10 +0630

    CMW : Import Assets and added System.Drawing.dll file.

---
+++ Assets/Gif.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+
+public class Gif : MonoBehaviour
+{
+    public Image gifImage;
+    public float delay = 0.1f;
+
+    int frameCount = 0;
+    FrameDimension dimension;
+
+    public void loadGif(string filepath)
+    {
+        gifImage = Image.FromFile(filepath);
+        dimension = new FrameDimension(gifImage.FrameDimensionsList[0]);
+        frameCount = gifImage.GetFrameCount(dimension);
+    }
+
+    private static byte[] Bitmap2RawBytes(Bitmap bmp)
+    {
+        byte[] bytes;
+        byte[] copyToBytes;
+        BitmapData bitmapData;
+        IntPtr Iptr = IntPtr.Zero;
+
+        bytes = new byte[bmp.Width * bmp.Height * 4];
+        copyToBytes = new byte[bmp.Width * bmp.Height * 4];
+
+        bmp.RotateFlip(RotateFlipType.RotateNoneFlipX);
+        Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+        bitmapData = bmp.LockBits(rect, ImageLockMode.ReadOnly, bmp.PixelFormat);
+        Iptr = bitmapData.Scan0;
+        Marshal.Copy(Iptr, bytes, 0, bytes.Length);
+
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            copyToBytes[bytes.Length - 1 - i] = bytes[i];
+        }
+        bmp.UnlockBits(bitmapData);
+
+        return copyToBytes;
+    }
+
+    public List<Texture2D> GetFrames()
+    {
+        List<Texture2D> gifFrames = new List<Texture2D>(frameCount);
+        for (int i = 0; i < frameCount; i++)
+        {
+            gifImage.SelectActiveFrame(dimension, i);
+            PropertyItem item = gifImage.GetPropertyItem(0x5100);
+            int frameDelay = (item.Value[0] + item.Value[1] * 256) * 10;
+            delay = frameDelay / 1000f;
+            var frame = new Bitmap(gifImage.Width, gifImage.Height);
+            System.Drawing.Graphics.FromImage(frame).DrawImage(gifImage, Point.Empty);
+            Texture2D texture = new Texture2D(frame.Width, frame.Height, TextureFormat.ARGB32, false);
+            texture.LoadRawTextureData(Bitmap2RawBytes(frame));
+            texture.Apply();
+            gifFrames.Add(texture);
+        }
+        return gifFrames;
+    }
+}
---
+++ Assets/GifImageDraw.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Configuration;
+
+public class GifImageDraw : MonoBehaviour
+{
+    public string gifPath = "";
+    // This is the UI image object you created which is a child of "canvas".
+    public GameObject imageGo;
+
+    //private void Start()
+    //{
+    //    GrabGifImage();
+    //}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GrabGifImage();
+        }
+    }
+    void GrabGifImage()
+    {
+        ShowGif(gifPath);
+    }
+
+    void ShowGif(string path)
+    {
+        GameObject imageGo = GameObject.Find("RawImage");
+        Gif gif = new Gif();
+        gif.loadGif(path);
+        RectTransform rect = imageGo.GetComponent<RectTransform>();
+        List<Texture2D> frames = gif.GetFrames();
+        RawImage rawImage = imageGo.GetComponent<RawImage>();
+        StartCoroutine(ShowGifFrames(rawImage, frames, gif.delay));
+    }
+
+    IEnumerator ShowGifFrames(RawImage rawImage, List<Texture2D> frames, float delay)
+    {
+        if (delay < 0.05f)
+            delay = 0.05f;
+        // Go for 5 iterations for clarity
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (Texture2D frame in frames)
+            {
+                rawImage.texture = frame as Texture;
+                yield return new WaitForSeconds(delay);
+            }
+        }
+    }
+}
