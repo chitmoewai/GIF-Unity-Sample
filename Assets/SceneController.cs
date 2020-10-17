53a689f549b9fa775d4b7937900a03c1a0fc79b1
Chit Moe Wai <chitmoewai.360ed@gmail.com>
2020-10-17 19:41:24 +0630

    CMW : load Gifs file from Addressable

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class SceneController : MonoBehaviour
 {
+    public AssetReference data;
 
     public Transform parent;
-    public GIF_Scriptable gif_data;
+    //public GIF_Scriptable gif_data;
 
 
     void Start()
     {
-        Instantiate(gif_data.gifObj[0], parent);
+        data.LoadAssetAsync<GIF_Scriptable>().Completed += GifsDownload_Completed;
+        //Instantiate(gif_data.gifObj[0], parent);
     }
 
-  
+    private void GifsDownload_Completed(AsyncOperationHandle<GIF_Scriptable> obj)
+    {
+        var result = obj.Result;
+
+        Instantiate(result.gifObj[0], parent);
+    }
 }
